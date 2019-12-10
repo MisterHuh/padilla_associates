afdb612dd0006a649a63dc10c187bf4a1afe59cb
MisterHuh <jaehuh86@yahoo.com>
2019-12-10 13:03:52 -0800

    added regex

--- main.js
+++ main.js
@@ -34,4 +34,34 @@ function addUser() {
   console.log("firstName is: ", firstName);
   console.log("lastName is: ", lastName);
   console.log("email is: ", email);
+
+  emailChecker(email);
+  nameChecker(firstName, lastName);
+
+  // var emailRegex = "/[^@]+@[^\.]+\..+/";
+  // var emailChecker = emailRegex.test(email);
+  // console.log("emailChecker is: ", emailChecker);
+}
+
+function nameChecker(firstName, lastName) {
+  if (firstName.length > 1 && lastName.length > 1) {
+    if ((/^[a-z ,.'-]+$/i.test(firstName)) && /^[a-z ,.'-]+$/i.test(lastName)) {
+      console.log("passed name test");
+      return true;
+    } else {
+      console.log("failed name test");
+    }
+  } else {
+    console.log("failed name test: need at least 1 character");
+  }
+}
+
+function emailChecker(email) {
+  // if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email)) {
+  //   console.log("passed");
+  // } else {
+  //   console.log("failed")
+  // }
+  console.log("passed email test")
+  return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email)
 }
